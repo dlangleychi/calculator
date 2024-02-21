94edd54a8eba86dcaa68c9fdef9edc463e4b1121
Dan Langley <dlangley812@hotmail.com>
2024-02-20 19:15:40 -0600

    Add global variables for operator and arguments

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+let num1 = undefined;
+let op = undefined;
+let num2 = undefined;
