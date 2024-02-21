a5b7b442679c33ed3d63a9ece5d6357af33fd7a0
Dan Langley <dlangley812@hotmail.com>
2024-02-20 19:22:52 -0600

    Add operate function

--- main.js
+++ main.js
@@ -5,5 +5,20 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 let num1 = undefined;
-let op = undefined;
+let operator = undefined;
 let num2 = undefined;
+
+const operate = (op, num1, num2) => {
+    switch(op) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'ERROR';
+    }
+}
